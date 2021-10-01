1de5a86264197817a3c96ef226aadc969ead137f
blfernandezGIT <blfernandez@up.edu.ph>
2021-10-01 19:30:08 +0800

    push

--- src/components/Home/LoadingScreen.js
+++ src/components/Home/LoadingScreen.js
@@ -2,13 +2,13 @@ import loadingImage from '../Assets/Images/sakura.gif'
 
 const LoadingScreen = () => {
     let bgColor;
-    const colorArray = ['bg-custom', 'bg-lightGrey', 'bg-yellowishWhite']
-    const randomIndex = Math.floor(Math.random() * 3)
+    const colorArray = ['bg-custom', 'bg-dirtyWhite']
+    const randomIndex = Math.floor(Math.random() * 2)
     bgColor = colorArray[randomIndex]
 
     return (
         <div className={`flex justify-center items-center w-full h-full ${bgColor} absolute z-50`}>
-            <img src={loadingImage} alt="loading..."/>
+            <img src={loadingImage} alt="loading..." className="h-1/2"/>
         </div>
     )
 }
