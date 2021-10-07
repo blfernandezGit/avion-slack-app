ae858a8e5e87a0421894828343c7009a3f29b718
blfernandezGIT <blfernandez@up.edu.ph>
2021-10-07 22:35:08 +0800

    channel create more options

--- src/components/Client/Menu/Channels/CreateChannel.js
+++ src/components/Client/Menu/Channels/CreateChannel.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useContext } from 'react'
+import { useState, useRef, useContext, useEffect } from 'react'
 import { postAPI } from '../../../../helpers/useAxiosPost'
 import { channelsBaseUrl, channelCreateAuditText } from '../../../../helpers/constants'
 import { ClientContext } from '../../../../context/ClientContext'
@@ -12,8 +12,12 @@ const CreateChannel = () => {
 
     const history = useHistory()
 
-    const [errorMessage, setErrorMessage] = useState(null)
-    const [ successMessage, setSuccessMessage] = useState(null)
+    const [ errorMessage, setErrorMessage ] = useState( null )
+    const [ successMessage, setSuccessMessage ] = useState( null )
+
+    const [ rows, setRows ] = useState('2')
+    const [ displayUsers, setDisplayUsers ] = useState( false )
+    const [ options, setOptions ] = useState('More Options')
 
     // Function to submit a post request for creating a new channel with user
     const createChannel = (e) => {
@@ -42,13 +46,33 @@ const CreateChannel = () => {
         channelNameRef.current.value = null
     }
 
+    const handleMoreOptions = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        if ( displayUsers ) {
+            setRows('2')
+            setDisplayUsers( false )
+            setOptions('More Options')
+        } else {
+            setRows('4')
+            setDisplayUsers(true)
+            setOptions('Less Options')
+        }
+    }
+
     return (
         <>
-            <form className="grid grid-cols-12 w-full" onSubmit={(e) => createChannel(e)}>
+            <form className={`grid grid-cols-12 grid-rows-${rows} w-full`} onSubmit={(e) => createChannel(e)}>
                 <input required className="border-2 border-pink col-span-11 focus:border-pink border-opacity-25 focus:outline-none rounded-md my-2 px-2" type="text" ref={channelNameRef} placeholder="Channel Name"/>
                 <button className="col-span-1 text-2xl text-pink hover:text-pink text-opacity-50" type="submit">
                     +
                 </button>
+                <button onClick={(e)=> handleMoreOptions(e)} className="col-span-full row-start-2 flex justify-start align-top text-sm text-pink text-opacity-50 hover:text-pink">{options}</button>
+
+                {displayUsers &&
+                    <div>try</div>
+                }
+
             </form>
             { successMessage &&  <Error errorMessage={successMessage}/> }
             
